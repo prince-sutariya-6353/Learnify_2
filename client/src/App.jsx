fdc5effe139a38fc36b1b972725f33d7cd897cdc
prince-sutariya-6353 <princegithub6353@gmail.com>
2025-06-16 14:02:17 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
 
@@ -26,7 +25,6 @@ function App() {
       <Routes>
         <Route path="/" element={<h1 className="text-center mt-10 text-3xl">Welcome to Learnify<br/>{message}</h1>} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
